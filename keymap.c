fb1cd15cdea875e907004f5ff0ffdc35ad9d4e30
zawnk <zawnk@users.noreply.github.com>
2020-01-05 22:00:46 +0100

    Edit mouse btns in media layer

--- keymap.c
+++ keymap.c
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_TRNS,         KC_TRNS,     KC_TRNS,     KC_MS_U,      KC_TRNS,    KC_TRNS,      KC_TRNS,
                    KC_TRNS,         KC_TRNS,     KC_MS_L,     KC_MS_D,      KC_MS_R,    KC_TRNS,
                    KC_TRNS,         KC_TRNS,     KC_TRNS,     KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,
-                   KC_TRNS,         KC_TRNS,     KC_TRNS,     KC_BTN1,      KC_BTN2,
+                   KC_TRNS,         KC_TRNS,     KC_TRNS,     KC_WBAK,      KC_WFWD,
                                                                                         KC_TRNS,      KC_TRNS,
                                                                                                       KC_TRNS,
-                                                                            KC_WBAK,    KC_TRNS,      KC_TRNS,
+                                                                            KC_BTN1,    KC_BTN2,      KC_TRNS,
                    // right hand
                    KC_TRNS,         KC_TRNS,     KC_TRNS,     KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,
                    KC_TRNS,         KC_TRNS,     KC_TRNS,     KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,
@@ -75,6 +75,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  KC_VOLU,     KC_VOLD,      KC_MUTE,    KC_TRNS,      KC_TRNS,
                    KC_TRNS,         KC_TRNS,
                    KC_TRNS,
-                   KC_TRNS,         KC_TRNS,     KC_WFWD
+                   KC_TRNS,         KC_TRNS,     KC_TRNS
                  )
 };
