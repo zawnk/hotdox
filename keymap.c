8426c02a8b01311d44c6e9688ae8b83d660c6233
zawnk <zonkor@gmail.com>
2020-01-12 20:56:16 +0100

    Change TD_CAPS to mod-tap key

--- keymap.c
+++ keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_ESC,          KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        LGUI(KC_TAB),
                    KC_GRV,          KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_NO,
                    LCTL_T(KC_TAB),  KC_A,        KC_S,        KC_D,        KC_F,        KC_G,
-                   TD(TD_CAPS),     KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        KC_NO, 
+                   LSFT_T(KC_CAPS), KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        KC_NO, 
                    KC_NO,           KC_NO,       KC_LGUI,     KC_LALT,     KC_NO,
                                                                                         TG(MDIA),    KC_INS,
                                                                                                      KC_MUTE,
@@ -85,6 +85,8 @@ uint16_t get_tapping_term(uint16_t keycode) {
         case LCTL_T(KC_TAB):
             return TAPPING_TERM - 75;
         case RALT_T(KC_RGUI):
+            return TAPPING_TERM - 75;
+        case LSFT_T(KC_CAPS):
             return TAPPING_TERM - 75;
         default:
             return TAPPING_TERM;
